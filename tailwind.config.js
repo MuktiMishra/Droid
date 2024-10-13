1e5b6cb91438de6b8df69f30503f3c5dd93e5bb0
mukti <muktimishra0495@gmail.com>
2024-10-13 09:20:06 +0530

    Homepage and other update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this if you have different file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
